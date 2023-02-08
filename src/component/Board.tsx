30e63c20ae0154a5adc7e14b3eba2e62c7cfe375
kosuke-nt <n.kosuke0906@gmail.com>
2023-02-08 10:55:24 +0900

    react-tutorial: ゲーム盤のサイズ変数化

--- src/component/Board.tsx
+++ src/component/Board.tsx
@@ -9,53 +9,27 @@ type BoardProps = {
 
 export const Board = (props: BoardProps) => {
 
-  const rowArray = Array(props.size)
-  const columnArray = Array(props.size)
+  const rowArray = [...Array(props.size)].map((_, i) => i)
+  const columnArray = [...Array(props.size)].map((_, i) => i)
 
-  // const renderSquare = (index: number) => {
-  //   return(
-  //     <Square
-  //       value={props.squares[index]}
-  //       onClick={()=> props.onClick(index)}
-  //     />
-  //   )
-  // }
-
-  // ボードの列とスクエアをloopを回すかmapを使って反復することで生成する
-
-  const squareGenerate = (index: number) => {
-    columnArray.map(num=>{
-      return(
-        <Square
-          value={props.squares[props.size*index + num]}
-          onClick={()=> props.onClick(props.size*index + num)}
-        />
-      )
-    })
-  }
+  const board = rowArray.map(row=> {
+    return(
+      <div className="board-row">
+        {columnArray.map(column=> {
+          return(
+            <Square
+              value={props.squares[props.size*row + column]}
+              onClick={()=>props.onClick(props.size*row + column)}
+            />
+          )
+        })}
+      </div>
+    )
+  })
 
   return (
-    // <div>
-    //   <div className="board-row">
-    //     {renderSquare(0)}
-    //     {renderSquare(1)}
-    //     {renderSquare(2)}
-    //   </div>
-    //   <div className="board-row">
-    //     {renderSquare(3)}
-    //     {renderSquare(4)}
-    //     {renderSquare(5)}
-    //   </div>
-    //   <div className="board-row">
-    //     {renderSquare(6)}
-    //     {renderSquare(7)}
-    //     {renderSquare(8)}
-    //   </div>
-    // </div>
-      rowArray.map(num=> {
-        <div className="board-row">
-          {squareGenerate(num)}
-        </div>
-      })
+    <div>
+      {board}
+    </div>
   )
 }
