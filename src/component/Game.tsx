0d5bb37a7ea3f32065686b2272c6206145a56c25
kosuke-nt <n.kosuke0906@gmail.com>
2023-02-09 10:35:11 +0900

    react-tutorial: 勝利条件、勝敗判定ロジック

--- src/component/Game.tsx
+++ src/component/Game.tsx
@@ -7,54 +7,14 @@ type GameProps = {
   size: number
 }
 
-const winnerOf3 = [
-                    [0, 1, 2],
-                    [3, 4, 5],
-                    [6, 7, 8],
-                    [0, 3, 6],
-                    [1, 4, 7],
-                    [2, 5, 8],
-                    [0, 4, 8],
-                    [2, 4, 6],
-                  ]
-
-const winnerOf4 = [
-                    [0, 1, 2, 3],
-                    [4, 5, 6, 7],
-                    [8, 9, 10, 11],
-                    [12, 13, 14, 15],
-                    [0, 4, 8, 12],
-                    [1, 5, 9, 13],
-                    [2, 6, 10, 14],
-                    [3, 7, 11, 15],
-                    [0, 5, 10, 15],
-                    [3, 6, 9, 12],
-                  ]
-
-const winnerOf5 = [
-                    [0, 1, 2, 3, 4],
-                    [5, 6, 7, 8, 9],
-                    [10, 11, 12, 13, 14],
-                    [15, 16, 17, 18, 19],
-                    [20, 21, 22, 23, 24],
-                    [0, 5, 10, 15, 20],
-                    [1, 6, 11, 16, 21],
-                    [2, 7, 12, 17, 22],
-                    [3, 8, 13, 18, 23],
-                    [4, 9, 14, 19, 24],
-                    [0, 6, 12, 18, 24],
-                    [4, 8, 12, 16, 20],
-                  ]
-
 export const Game = (props: GameProps) => {
-  const size = 5
-  const squares: squares = Array(size^2).fill(null)
+  const squares: squares = Array(props.size^2).fill(null)
   const [history, setHistory] = React.useState<squares[]>([squares])
   const [stepNumber, setStepNumber] = React.useState(0)
   const [xIsNext, setXIsNext] = React.useState(true)
   const [current, setCurrent] = React.useState(squares)
 
-  const winner = calculateWinner(size, current)
+  const winner = calculateWinner(props.size, current)
   let status: string
   if (winner) {
     status = 'Winner: ' + winner;
@@ -100,7 +60,7 @@ export const Game = (props: GameProps) => {
           <Board
             squares={current}
             onClick={(i: number) => handleClick(i)}
-            size={size}
+            size={props.size}
           />
         </div>
         <div className="game-info">
@@ -112,37 +72,45 @@ export const Game = (props: GameProps) => {
 
 }
 
+// サイズを入力して勝利条件を設定する
+const setWinningArray = (size: number): number[][] => {
+  let rowWinning: number[][] = []
+  for(let i = 0; i < size; i++) {
+    let rowWinningArray = [...Array(size)].map((_, j)=> size*i+j)
+    rowWinning.push(rowWinningArray)
+  }
+
+  let columnWinning: number[][] = []
+  for(let i = 0; i < size; i++) {
+    let columnWinningArray = [...Array(size)].map((_, j)=> size*j+i)
+    columnWinning.push(columnWinningArray)
+  }
+
+  let crossWinning:number[][] = []
+  crossWinning.push([...Array(size)].map((_, i)=> (size+1)*i))
+  crossWinning.push([...Array(size)].map((_, i)=> (size-1)*(i+1)))
+
+  let winningArray = rowWinning.concat(columnWinning, crossWinning)
+  return(winningArray)
+}
+
 // ========================================
-function calculateWinner(size:number, squares: squares) {
-  let lines = [[0]]
-  switch(size) {
-    case 3:
-      lines = winnerOf3
-      for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i]
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-          return squares[a];
-        }
+// 
+const  calculateWinner = (size:number, squares: squares) => {
+  let lines: number[][] = setWinningArray(size)
+  let testArray: number[] = Array(size)
+  for(let i = 0; i < lines.length; i ++) {
+    testArray = lines[i]
+    let winCount = 0
+    for(let j = 1; j < testArray.length; j ++) {
+      if(squares[testArray[0]] && squares[testArray[0]] === squares[testArray[j]]) {
+        winCount++
       }
-      break
-    case 4:
-      lines = winnerOf4
-      for (let i = 0; i < lines.length; i++) {
-        const [a, b, c, d] = lines[i]
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d]) {
-          return squares[a];
-        }
-      }
-      break
-    case 5:
-      lines = winnerOf5
-      for (let i = 0; i < lines.length; i++) {
-        const [a, b, c, d, e] = lines[i]
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a] === squares[d] && squares[a] === squares[e]) {
-          return squares[a];
-        }
-      }
-      break
+    }
+
+    if(winCount === size - 1) {
+      return squares[testArray[0]]
+    }
   }
   return null
 }
